62a04a2d2e944783a89ad09585b86904b42199a3
YeezyTheDev <adewoleoluwasegun7@gmail.com>
2026-02-04 08:39:45 +0100

    first commit

--- Week 3/script.js
+++ Week 3/script.js
@@ -2,37 +2,65 @@ const searchButton = document.querySelector(".search-btn");
 const recipeSection = document.querySelector(".recipe-section");
 const recipeNameInput = document.querySelector(".recipe-search");
 console.log(searchButton);
-async function fetchRecipes() {
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeNameInput.value}`;
-
-    try {
-        const response = await fetch(url,{
-        method: 'GET',
-        headers : {
-        'Accept': 'application/json'
+let loadingState = false;
+searchButton.addEventListener('click', function(){
+    async function fetchRecipes() {
+        loadingState = true;
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${recipeNameInput.value}`;
+        try {
+            const response = await fetch(url,{
+            method: 'GET',
+            headers : {
+            'Accept': 'application/json'
+            }
+            });
+            if (!response.ok) {
+                throw new Error(`${response.status}`);
+            }
+            const Datas = await response.json();
+            console.log(Datas.meals);
+            return Datas.meals;
+        } catch (error) {
+            console.log(error.message);
+    
         }
-        });
-        if (!response.ok) {
-            throw new Error(`${response.status}`);
-        }
-        const Datas = await response.json();
-        //console.log(Datas.meals);
-        return Datas.meals;
-    } catch (error) {
-        console.log(error.message);
+    }
+    fetchRecipes();
 
+    loadingState = false;
+    async function getResults(){
+        const results = await fetchRecipes();
+        console.log(results);
+    if(results && !loadingState){
+      const recipes = results.map(result => 
+        `<div
+        style ="
+        background-image: url('${result.strMealThumb}');
+        width: 100;
+        background-position: center;
+        background-size: cover;
+        object-fit: contain;
+        min-height: 100vh;
+        "
+        class="recipe-card">
+        <h3 class="title">${result.strMeal}</h3>
+        <p>Category:${result.strCategory}</p>
+        </div>`
+        
+        ).join('');
+        recipeSection.innerHTML = recipes;
+    }else if(!results && !loadingState){
+        const nullMessage = 'No results for your search';
+        recipeSection.innerHTML = nullMessage;
+    }else if(loadingState){
+        const loadingMessage = 'loading...';
+        recipeSection.innerHTML = loadingMessage;
     }
 }
-searchButton.addEventListener('click', fetchRecipes)
-//fetchRecipes();
-async function getResults(){
-    const results = await fetchRecipes();
-    //console.log(results);
+    getResults();
+    loadingState = false;
+    console.log(loadingState);
+    
+})
 
-  const recipes = results.map(result => 
-    `<p class="title">${result.strMeal}</p>`
-    ).join('');
-    recipeSection.innerHTML = recipes;
-}
-getResults();
 
